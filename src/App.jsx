1f0784d7525289afd1e975aee3582b6e49b879aa
nikunj Rohit <nikunjrohit10@gmail.com>
2024-04-27 06:38:26 +0000

    update quote api with error handling

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,39 @@ import './App.css'
 import Quote from './components/quote'
 
 function App() {
+  const [quoteData, setQuoteData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  const [quoteData , setQuoteData] = useState(null);
-  
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch('https://dummyjson.com/quotes/random');
-      const data = await response.json();
-      setQuoteData(data);
+      setLoading(true);
+      try {
+        const response = await fetch('https://dummyjson.com/quotes/random');
+        const data = await response.json();
+        setQuoteData(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
     }
 
     fetchData();
-  },[])
-  
+  }, []);
+
   return (
     <div className="App">
-      {quoteData && <Quote quote={quoteData.quote} author={quoteData.author} />}
-  </div>
-  )
+      {loading? (
+        <p>Loading...</p>
+      ) : (
+        quoteData && (
+          <Quote quote={quoteData.quote} author={quoteData.author} />
+        )
+      )}
+      {error && <p>Error: {error}</p>}
+    </div>
+  );
 }
 
-export default App
+export default App;
